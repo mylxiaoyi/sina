1ea7749b7bbff3b684edbba258b8c53892e74b51
mylxiaoyi <mylxiaoyi@gmail.com>
2016-03-02 12:11:42 +0800

    backup

--- sina/pipelines.py
+++ sina/pipelines.py
@@ -4,21 +4,31 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
 
 
 class SinaPipeline(object):
-    def __init__(self):
-        self.file = open('items.txt', 'w')
+
+    collection_name = 'sina_items'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+        )
 
     def process_item(self, item, spider):
-        print >> self.file, item['link'].encode('utf-8')
-        print >> self.file, item['title'].encode('utf-8')
-        print >> self.file, item['pub_date'].encode('utf-8')
-        print >> self.file, item['body'].encode('utf-8')
+        self.db[self.collection_name].insert(dict(item))
         return item
 
     def open_spider(self, spider):
-        pass
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
 
     def close_spider(self, spider):
-        self.file.close()
+        self.client.close()
